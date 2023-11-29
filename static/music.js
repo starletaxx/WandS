b118bd14a056eba075816bb5e0078d4c1fb93d79
starletaxx <61972433+starletaxx@users.noreply.github.com>
2023-11-29 17:15:16 +0800

    Add files via upload

---
+++ static/music.js
@@ -0,0 +1,24 @@
+const bgm = uni.createInnerAudioContext();
+bgm.src = '../static/music.mp3'
+bgm.loop = true;  //循环播放
+// bgm.autoplay=true;  //自动播放
+bgm.sessionCategory = 'soloAmbient'; //在后台时不播放，如有其他播放，会暂停其他播放（但在移动端h5中 后台不播放失效）（其他属性自行百度）
+bgm.volume = 0.4  //音量
+var music = {
+	//mute 表示是否是静音，，默认静音
+	playBgm({
+		mute = true
+	}) {
+		if (!bgm) return;
+		if (mute) {
+			bgm.pause()
+		} else {
+			bgm.play()
+		}
+ 
+		bgm.onPause(() => {})
+		bgm.onPlay(() => {})
+		bgm.onError((res) => {})
+	}
+}
+module.exports = music
